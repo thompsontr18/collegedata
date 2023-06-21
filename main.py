4748edb60774ea9fe8425cc0c8d059ee026b44b9
thompsontr18 <thompsontr18@gmail.com>
2023-06-21 09:10:25 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#code here
+from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from openpyxl import load_workbook
+
+
+
+def googlesearch():
+    query = input("What would you like to search for? ")
+    for j in search(query, tld="co.in", stop=1, pause=2):
+        return j
+
+
+url = googlesearch()
+print(url)
+html = requests.get(url)
+soup = BeautifulSoup(html.text, "html.parser")
+print(soup.text)
+
+
+
