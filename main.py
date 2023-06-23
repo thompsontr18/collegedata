d34ac45bc3e66581759370a8509128250c87206f
thompsontr18 <thompsontr18@gmail.com>
2023-06-23 10:15:42 -0400

    main.py supports re

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from googlesearch import search
-import spacy
-import en_core_web_sm
+import re
 
 
 def googlesearch(query):
     for j in search(query, tld="com", stop=1, pause=2):
         return j
 
-'''
+
 cname = input("Enter the name of a college: ")
-cname = "petersons college search" + cname
+cname = cname + " faculty and staff"
 
 collegewebsite = googlesearch(cname)
 print(collegewebsite)
@@ -20,25 +19,21 @@ print(collegewebsite)
 
 html = requests.get(collegewebsite)
 soup = BeautifulSoup(html.text, "html.parser")
-print(soup.text)
-listofmajors = soup.findAll("th", attrs={"class": "top-level"})
-
-for major in listofmajors:
-    print(major.text)
-'''
+text = soup.text
 
 
+phonenumber = r'\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}'
+phonenumbermatches = []
+email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+emailmatches = []
 
-nlp = en_core_web_sm.load()
-text = ("When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to, said Thrun, in an interview with Recode earlier this week.")
-doc = nlp(text)
+phonematch = re.findall(phonenumber, text)
+if phonematch:
+    phonenumbermatches.extend(phonematch)
+emailmatch = re.findall(email, text)
+if emailmatch:
+    emailmatches.extend(emailmatch)
 
-#gets parts of speech
-print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-print("\n")
-print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
-print("\n")
+for pnm, em in zip(phonenumbermatches, emailmatches):
+    print(pnm,em)
 
-#understands people, places, things, etc
-for entity in doc.ents:
-    print(entity.text, entity.label_)
