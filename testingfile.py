0f810169e5269541be095b5edf46ed51147092f7
thompsontr18 <thompsontr18@gmail.com>
2023-06-27 11:20:30 -0400

    changed file

--- testingfile.py
+++ testingfile.py
@@ -10,30 +10,23 @@ def googlesearch(query):
         return j
 
 
-cname = input("Enter the name of a college: ")
-
 
 df = pd.read_excel('data.xlsx', usecols='A,B,C')
 
 
 for i in range(len(df)):
-    if str(df.iloc[i, 0]) == cname.title():
+    srch = str(df.iloc[i, 0]) + " " + str(df.iloc[i, 1]) + " " + str(df.iloc[i, 2]) 
+    link = googlesearch(srch)
+    print(df.iloc[i, 1])
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, "html.parser")
+    text = soup.text
 
-        srch = str(df.iloc[i, 0]) + " " + str(df.iloc[i, 1]) + " " + str(df.iloc[i, 2]) 
-        #print(srch)
-
-
-        link = googlesearch(srch)
-        print(link)
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text, "html.parser")
-        text = soup.text
-
-
-        email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
-        emailmatches = []
-        emailmatches = re.findall(email, text)
+    email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+    emailmatches = []
+    emailmatches = re.findall(email, text)
 
 
-        for em in emailmatches:
-            print(em)
+    for em in emailmatches:
+        print(em)
+    print('-----------------------------------')
