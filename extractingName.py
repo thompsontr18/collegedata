f58d9345d20a75a25ef347b85a14eafbe3d54205
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-06-29 16:09:19 -0400

    removed unnecessary files

--- extractingName.py
+++ extractingName.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 from googlesearch import search
 import re
+from openpyxl import load_workbook
+
 
 
 def googlesearch(query):
@@ -12,15 +14,16 @@ def googlesearch(query):
 
 
 df = pd.read_excel('data.xlsx', usecols='A,B,C')
-
+df_print = pd.DataFrame(columns=['Email IDs','Other Details'])
+book = load_workbook('dataWithEmail.xlsx')
+sheet=book.worksheets[0]
 
 major = ""
 collegeandmajor = {}
 for i in range(len(df)):
     srch = str(df.iloc[i, 0]) + " " + str(df.iloc[i, 1])
-    if df.iloc[i,0]=="Albany Technical College" or df.iloc[i,0]=="Andrew College":
-        continue
     if (df.iloc[i, 0], df.iloc[i, 1]) in collegeandmajor:
+        df_print.loc[len(df_print.index)] = collegeandmajor[(df.iloc[i, 0], df.iloc[i, 1])]
         continue
     srch += " program contact"
     link = googlesearch(srch)
@@ -28,7 +31,7 @@ for i in range(len(df)):
         print('-----------------------------------')
         print(df.iloc[i, 1])
         major = df.iloc[i, 1]
-        collegeandmajor[(df.iloc[i, 0], df.iloc[i, 1])] = 1
+        # collegeandmajor[(df.iloc[i, 0], df.iloc[i, 1])] = 1
 
 
     html = requests.get(link)
@@ -40,9 +43,19 @@ for i in range(len(df)):
     emailmatches = re.findall(emailre, text)
 
 
+    strings = ""
+    for i in range(len(text)):
+        # strings += text[i]
+        if "@" in text[i]:
+            # strings = strings[-150:]
+            strings = text[i-150:i+50]
+            print(strings)
+
+
     if len(emailmatches) == 0:
         print("N/A")
-        print("No Contact Details")
+        df_print.loc[len(df_print.index)] = ["N/A",strings]
+        collegeandmajor[(df.iloc[i, 0], df.iloc[i, 1])] = ["N/A",strings]
         continue
     printedtext = ""
     for i in range(0,len(emailmatches)):
@@ -51,6 +64,12 @@ for i in range(len(df)):
         else:
             printedtext += emailmatches[i]
     print(printedtext)
-    for i in range(0,len(emailmatches)):
-        details=text.split(emailmatches[i])[0]
-        print(details[-150:])
+    df_print.loc[len(df_print.index)] = [printedtext,strings]
+    collegeandmajor[(df.iloc[i, 0], df.iloc[i, 1])] = [printedtext,strings]
+with pd.ExcelWriter('dataWithEmail.xlsx',mode='a', if_sheet_exists='overlay') as writer:  
+    df_print.to_excel(writer,sheet_name="Sheet",header=True, index=False, startrow=sheet.max_row)
+
+
+
+
+
