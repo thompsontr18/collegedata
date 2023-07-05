174af791568bff17089c80e7cf5889cb76196160
thompsontr18 <thompsontr18@gmail.com>
2023-07-05 12:04:25 -0400

    start of nlp

--- namerec.py
+++ namerec.py
@@ -4,3 +4,6 @@ from spacy import displacy
 from collections import Counter
 import en_core_web_sm
 nlp = en_core_web_sm.load()
+
+doc = nlp('The random guy took Tilly and Daisy on a walk to Andrew Jones College. After that, John Smith went shopping')
+print([(X.text, X.label_) for X in doc.ents])
